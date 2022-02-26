6567ab249bc4a0df507e0740e8bf39fe02cabbda
Robert Baumgartner <robert.baumgartner@htlwienwest.at>
2022-02-26 11:55:24 +0100

    inital setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container d-flex flex-column align-items-center">
+    <h2>
+      <span class="text-danger"><i class="far fa-heart"></i></span>
+      {{ msg }}
+      <span class="text-danger"><i class="far fa-heart"></i></span>
+    </h2>
+    <img src="@/assets/logo.svg" alt="Vue logo" height="100" />
+    <SayHello name="Robert" @greeting="showGreeting"></SayHello>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SayHello from '@/components/SayHello.vue';
+
+const msg = 'Willkommen!';
+
+const showGreeting = (payload: string) => alert(payload);
+</script>
+
+<style></style>
---
+++ src/components/SayHello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p class="fs-5 fst-italic">{{ name }} sagt: Hallo!</p>
+    <button @click="emit('greeting', 'Hi!')" class="btn btn-primary mx-auto d-block">Greet Back</button>
+  </div>
+</template>
+<script setup lang="ts">
+interface Props {
+  name: string;
+}
+
+interface Emits {
+  (event: 'greeting', msg: string): void;
+}
+
+defineProps<Props>();
+const emit = defineEmits<Emits>();
+</script>
